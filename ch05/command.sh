10a2836620fff623692585d555d11319ffe795fc
Mostafa Negim <mostafa.negim@gmail.com>
2021-05-05 21:36:19 +0430

    add: pod-with-container-command.yaml

--- ch05/command.sh
+++ ch05/command.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 kubectl --namespace kube-public create -f single-container-pod.yaml
+
+#Run the following command to change the namespace for all subsequent
+#requests by modifying the current context:
+kubectl config set-context $(kubectl config current-context) --namespace kube-public
